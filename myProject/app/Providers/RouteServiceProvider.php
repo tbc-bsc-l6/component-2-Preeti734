ccaf11bdc5f4b2c2952801c73b9d2a3a8912056f
Preeti734 <spreeti19@tbc.edu.np>
2023-01-07 15:02:06 +0545

    user and login authentication

--- myProject/app/Providers/RouteServiceProvider.php
+++ myProject/app/Providers/RouteServiceProvider.php
@@ -18,9 +18,21 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/dashboard';
+    public const ADMIN_HOME = '/admin/dashboard';
+
+    
+
+   /**
+     * The controller namespace for the application.
+     *
+     * When present, controller route declarations will automatically be prefixed with this namespace.
+     *
+     * @var string|null
+     */
+    protected $namespace = 'App\\Http\\Controllers';
 
     /**
-     * Define your route model bindings, pattern filters, and other route configuration.
+     * Define your route model bindings, pattern filters, etc.
      *
      * @return void
      */
--- myProject/routes/web.php
+++ myProject/routes/web.php
@@ -29,3 +29,12 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+//Admin
+Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function(){
+    Route::namespace('Auth')->group(function(){
+        //login route
+        Route::get('login','AuthenticatedSessionController@create')->name('login');
+
+    });
+});
