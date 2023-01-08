757366c4009f51f8b2d6a00bd795a81e468cef61
Preeti734 <spreeti19@tbc.edu.np>
2023-01-08 16:23:32 +0545

    Update on admin login code using middleware

--- myProject/app/Http/Controllers/AdminController.php
+++ myProject/app/Http/Controllers/AdminController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    
+    /**
+     * Display a listing of the resource.
+     * 
+     * @return \Illuminate\Http\Response
+    */
+
+    public function index()
+    {
+        return view ('admin.index');
+    }
+
+
 }
