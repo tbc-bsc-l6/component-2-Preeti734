43fbb519b05da3bccfd2bbdf0cf399245fc9c13b
Preeti734 <spreeti19@tbc.edu.np>
2023-01-17 17:24:29 +0545

    CRUD changes

--- myProject/resources/views/FrontEnd/master.blade.php
+++ myProject/resources/views/FrontEnd/master.blade.php
@@ -8,7 +8,6 @@
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Rokkitt:100,300,400,700" rel="stylesheet">
 	
-	{{ URL::asset('css/css.css') }}
 
 	<!-- Animate.css -->
 	<link rel="stylesheet" href="{{asset ('assets/css/animate.css')}}">
